dad95fe341686a232401e770c1a1c9a283090545
akboubhajar <hajarakboub@gmail.com>
2024-03-04 18:49:44 +0100

    v1

---
+++ src/main/java/com/hakboub/hospitalmanagement/entities/Patient.java
@@ -0,0 +1,13 @@
+package com.hakboub.hospitalmanagement.entities;
+
+import java.util.Date;
+
+public class Patient {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+
+}
